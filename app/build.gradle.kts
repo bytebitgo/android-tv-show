e2e618dce05e13dce91dd95001b40d1a3caecf69
ff <ff@null.com>
2025-03-26 14:01:38 +0800

    1

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.application)
+    kotlin("android") version "1.9.22"
+}
+
+android {
+    namespace = "com.cmcc.myapplication"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.cmcc.myapplication"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.leanback)
+    implementation(libs.glide)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ app/src/main/java/com/cmcc/myapplication/WebViewActivity.kt
@@ -0,0 +1,34 @@
+package com.cmcc.myapplication
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.FragmentActivity
+
+class WebViewActivity : FragmentActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+
+        webView = findViewById(R.id.webview)
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            useWideViewPort = true
+            loadWithOverviewMode = true
+        }
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://mt5.gslb.vip/mt/")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+} 
