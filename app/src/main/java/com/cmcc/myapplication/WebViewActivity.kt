04158ea27643b61972c2144e422e6350c64f7f9d
bytebitgo <bytebit@foxmail.com>
2025-03-31 13:48:14 +0800

    更新版本号至 1.2.0，优化遥控器D-pad导航支持，添加自定义光标样式，优化页面滚动体验，并更新更新日志和 README 文档。

--- app/src/main/java/com/cmcc/myapplication/WebViewActivity.kt
+++ app/src/main/java/com/cmcc/myapplication/WebViewActivity.kt
@@ -2,12 +2,16 @@ package com.cmcc.myapplication
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.FragmentActivity
 
 class WebViewActivity : FragmentActivity() {
     private lateinit var webView: WebView
+    
+    // 滚动步长
+    private val SCROLL_STEP = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +29,38 @@ class WebViewActivity : FragmentActivity() {
         webView.webViewClient = WebViewClient()
         webView.loadUrl(BuildConfig.WEB_URL)
 
+        // 添加遥控器按键支持
+        webView.setOnKeyListener { _, keyCode, event ->
+            if (event.action != KeyEvent.ACTION_DOWN) return@setOnKeyListener false
+            
+            when (keyCode) {
+                KeyEvent.KEYCODE_DPAD_CENTER, KeyEvent.KEYCODE_ENTER -> {
+                    webView.performClick()
+                    true
+                }
+                KeyEvent.KEYCODE_DPAD_UP -> {
+                    webView.scrollBy(0, -SCROLL_STEP)
+                    true
+                }
+                KeyEvent.KEYCODE_DPAD_DOWN -> {
+                    webView.scrollBy(0, SCROLL_STEP)
+                    true
+                }
+                KeyEvent.KEYCODE_DPAD_LEFT -> {
+                    if (webView.canGoBack()) {
+                        webView.goBack()
+                        true
+                    } else false
+                }
+                KeyEvent.KEYCODE_DPAD_RIGHT -> {
+                    if (webView.canGoForward()) {
+                        webView.goForward()
+                        true
+                    } else false
+                }
+                else -> false
+            }
+        }
     }
 
     override fun onBackPressed() {
